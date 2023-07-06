4df377a591710cfba8690cfdcea748b4d278916e
Lokeshsri11 <lokeshsri1109@gmail.com>
2023-07-06 13:33:02 +0530

    commit codes

---
+++ README.md
@@ -0,0 +1,9 @@
+# Text to SQL Converter
+  The Text to SQL Converter is a web application that allows users to convert natural language queries into SQL queries. It leverages the power of the spaCy library for natural language processing and Flask framework for building the web application.
+
+## Features
+   1. Convert natural language queries into SQL queries
+   2. Supports various SQL operations like SELECT, INSERT, UPDATE, DELETE, etc.
+   3. Extracts entities from the input text to generate dynamic SQL queries
+   4. User-friendly interface for inputting the text and viewing the generated SQL query
+   5. Easy integration with existing Python projects or web applications
---
+++ spaCy.py
@@ -0,0 +1,70 @@
+import spacy
+
+nlp = spacy.load('en_core_web_sm')
+
+def extractEntities(doc):
+    entities = []
+
+    # Extract entities based on desired patterns or rules
+    for token in doc:
+        if token.pos_ == 'PROPN':
+            entities.append({
+                'var_name': token.text.lower().replace(' ', '_'),
+                'type': 'string',
+                'description': f'{token.text} entity'
+            })
+
+    return entities
+
+def generateSQLQuery(entities):
+    operation = input("Enter the SQL operation (e.g., SELECT, INSERT, UPDATE, DELETE): ")
+    table = input("Enter the table name: ")
+    conditions = []
+
+    if operation.upper() == 'SELECT':
+        columns = input("Enter the column names (comma-separated): ")
+        query = f"SELECT {columns} FROM {table} WHERE "
+        for entity in entities:
+            var_name = entity['var_name']
+            value = input(f"Enter the value for {var_name}: ")
+            conditions.append(f"{var_name} = '{value}'")
+    elif operation.upper() == 'INSERT':
+        columns = input("Enter the column names (comma-separated): ")
+        values = input("Enter the values (comma-separated): ")
+        query = f"INSERT INTO {table} ({columns}) VALUES ({values})"
+    elif operation.upper() == 'UPDATE':
+        column = input("Enter the column name to update: ")
+        new_value = input("Enter the new value: ")
+        query = f"UPDATE {table} SET {column} = '{new_value}' WHERE "
+        for entity in entities:
+            var_name = entity['var_name']
+            value = input(f"Enter the value for {var_name}: ")
+            conditions.append(f"{var_name} = '{value}'")
+    elif operation.upper() == 'DELETE':
+        query = f"DELETE FROM {table} WHERE "
+        for entity in entities:
+            var_name = entity['var_name']
+            value = input(f"Enter the value for {var_name}: ")
+            conditions.append(f"{var_name} = '{value}'")
+    else:
+        print("Invalid SQL operation.")
+        return None
+
+    if conditions:
+        query += " AND ".join(conditions)
+
+    return query
+
+def convertTextToSQL(text):
+    doc = nlp(text)
+    entities = extractEntities(doc)
+    sqlQuery = generateSQLQuery(entities)
+
+    return sqlQuery
+
+# Example usage:
+text = input("Enter your text: ")
+sqlQuery = convertTextToSQL(text)
+if sqlQuery:
+    print("Generated SQL query:")
+    print(sqlQuery)
